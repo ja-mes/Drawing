1c517b3555ad8210a21b52973ef6ea94865a1817
James <jamezbrown100@gmail.com>
2016-12-01 12:38:24 -0600

    Replace view controller with homevc

---
+++ HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  Drawing
+//
+//  Created by James Brown on 12/1/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
