fbac564f4e5b1fbc3bbec80e30e1797baa8d1086
James <jamezbrown100@gmail.com>
2016-12-01 16:59:55 -0600

    Setup collectionView

---
+++ Drawing/ColorVC.swift
@@ -0,0 +1,34 @@
+//
+//  ColorVC.swift
+//  Drawing
+//
+//  Created by James Brown on 12/1/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class ColorVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
