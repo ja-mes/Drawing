077bd36d84132c5052383900706396f4e45049c0
James <jamezbrown100@gmail.com>
2016-12-01 12:35:14 -0600

    Add custom button class

---
+++ Drawing/CustomButton.swift
@@ -0,0 +1,17 @@
+//
+//  RoundedButton.swift
+//  Drawing
+//
+//  Created by James Brown on 12/1/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomButton: UIButton {
+    @IBInspectable var cornerRadius: Double = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
