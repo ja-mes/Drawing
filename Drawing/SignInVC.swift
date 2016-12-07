8781599c75ab74721d415060b1c8ad1726eb9bf6
James <jamezbrown100@gmail.com>
2016-12-07 16:50:06 -0600

    Add sign in and sign up view controllers

---
+++ Drawing/SignInVC.swift
@@ -0,0 +1,17 @@
+//
+//  SignInVC.swift
+//  Drawing
+//
+//  Created by James Brown on 12/7/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Drawing/SignUpVC.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpVC.swift
+//  Drawing
+//
+//  Created by James Brown on 12/7/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
