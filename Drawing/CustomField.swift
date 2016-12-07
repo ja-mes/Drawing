57bd3cbaed22fc99098ad1bfeda6c4416fd80278
James <jamezbrown100@gmail.com>
2016-12-07 16:22:16 -0600

    Add customfield to indent placeholder

---
+++ Drawing/CustomField.swift
@@ -0,0 +1,26 @@
+//
+//  CustomField.swift
+//  Drawing
+//
+//  Created by James Brown on 12/7/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class CustomField: UITextField {
+
+    let padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5);
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+}
