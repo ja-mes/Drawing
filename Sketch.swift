40ea82aef02a562c40b7254d8b4d804e0ebad632
James <jamezbrown100@gmail.com>
2016-12-11 08:18:55 -0600

    Create sketch class

---
+++ Sketch.swift
@@ -0,0 +1,21 @@
+//
+//  Sketch.swift
+//  Drawing
+//
+//  Created by James Brown on 12/11/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseStorage
+
+class Sketch {
+    var image: UIImage
+    
+    init(img: UIImage) {
+        image = img
+    }
+    
+}
