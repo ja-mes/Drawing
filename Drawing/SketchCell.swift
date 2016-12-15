1b42b95d0dadd3614fd5071e0bd8c9d4a04fa332
James <jamezbrown100@gmail.com>
2016-12-15 13:34:19 -0600

    Create new sketch cell class

---
+++ Drawing/SketchCell.swift
@@ -0,0 +1,13 @@
+//
+//  SketchCell.swift
+//  Drawing
+//
+//  Created by James Brown on 12/15/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class SketchCell: UITableViewCell {
+    @IBOutlet var sketchImageView: UIImageView!
+}
