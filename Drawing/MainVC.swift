0586cf1d5e6eff92094e201660fffcc57eb81cee
James <jamezbrown100@gmail.com>
2016-12-14 18:44:38 -0600

    Setup fetched results controller

--- Drawing/MainVC.swift
+++ Drawing/MainVC.swift
@@ -7,23 +7,37 @@
 //
 
 import UIKit
+import CoreData
 
-class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
     @IBOutlet weak var tableView: UITableView!
+    
+    var controller: NSFetchedResultsController<Sketch>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        
+        fetchSketches()
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
+        
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if let sections = controller.sections {
+            return sections[section].numberOfObjects
+        }
+        
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -32,6 +46,10 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
         
         return UITableViewCell()
+    }
+    
+    func fetchSketches() {
+        
     }
 
     @IBAction func plusButtonPressed(_ sender: Any) {
