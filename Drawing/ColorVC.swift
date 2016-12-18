cda470357854554f73a9e91695645f381b835555
James <jamezbrown100@gmail.com>
2016-12-18 13:28:23 -0600

    Add the new colors to the box

--- Drawing/ColorVC.swift
+++ Drawing/ColorVC.swift
@@ -17,6 +17,24 @@ class ColorVC: UIViewController {
         UIColor(red: 156/255, green: 31/255, blue: 176/255, alpha: 1.0),
         UIColor(red: 103/255, green: 53/255, blue: 183/255, alpha: 1.0),
         UIColor(red: 64/255, green: 78/255, blue: 181/255, alpha: 1.0),
+        
+        UIColor(red: 36/255, green: 148/255, blue: 243/255, alpha: 1.0),
+        UIColor(red: 12/255, green: 167/255, blue: 244/255, alpha: 1.0),
+        UIColor(red: 6/255, green: 188/255, blue: 212/255, alpha: 1.0),
+        UIColor(red: 2/255, green: 150/255, blue: 136/255, alpha: 1.0),
+        UIColor(red: 76/255, green: 176/255, blue: 80/255, alpha: 1.0),
+        
+        UIColor(red: 139/255, green: 196/255, blue: 74/255, alpha: 1.0),
+        UIColor(red: 205/255, green: 221/255, blue: 57/255, alpha: 1.0),
+        UIColor(red: 255/255, green: 237/255, blue: 59/255, alpha: 1.0),
+        UIColor(red: 255/255, green: 194/255, blue: 7/255, alpha: 1.0),
+        UIColor(red: 255/255, green: 153/255, blue: 0/255, alpha: 1.0),
+        
+        UIColor(red: 255/255, green: 88/255, blue: 34/255, alpha: 1.0),
+        UIColor(red: 121/255, green: 85/255, blue: 72/255, alpha: 1.0),
+        UIColor(red: 158/255, green: 158/255, blue: 158/255, alpha: 1.0),
+        UIColor(red: 96/255, green: 125/255, blue: 139/255, alpha: 1.0),
+        UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
     ]
 
     override func viewDidLoad() {
