590dcabdfd317c93de1a5de0eb0a6512ca322939
James <jamezbrown100@gmail.com>
2016-12-21 06:40:43 -0600

    Don't need this

--- Drawing/SketchCell.swift
+++ Drawing/SketchCell.swift
@@ -12,11 +12,4 @@ class SketchCell: UITableViewCell {
     @IBOutlet weak var dateLl: UILabel!
     @IBOutlet var sketchImageView: UIImageView!
     @IBOutlet weak var customView: CustomView!
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        // this line makes the custom view expand with the cell. the aspect ratio and the top and bottom constraints make this possible
-        customView.frame = CGRect(x: 0, y: 0, width: 1000, height: 1000)
-    }
 }
